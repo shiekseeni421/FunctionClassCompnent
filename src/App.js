09c6df0c89105d66374910cae21785fa85d5da34
shiekseeni421 <61022287+shiekseeni421@users.noreply.github.com>
2022-04-20 17:38:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import FormClassBase from "./Component/FormClassBased";
+import FormComponet from "./Component/FormFunctComponet";
+
+function App() {
+  return (
+    <div className="Main-continer">
+      <FormClassBase />
+      <FormComponet />
+    </div>
+  );
+}
+
+export default App;
